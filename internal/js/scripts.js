486d745b021134717e117e7fb1ff6de65487926a
Nelson Pecora <nelson@yoshokatana.com>
2013-04-02 15:01:22 -0700

    mostly done

---
+++ internal/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(window).bind('resize',function() {
+        $('.popover').css('height',$('.popover').width()/3.15);
+    });
+    $('.popover').css('height',$('.popover').width()/3.15);
+});
