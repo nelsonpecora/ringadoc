01a6bab9bfe63d61de3a78c8cda91c806249941e
Nelson Pecora <nelson@yoshokatana.com>
2013-04-03 16:14:50 -0700

    responsive styles added

--- internal/js/scripts.js
+++ internal/js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $(window).bind('resize',function() {
-        $('.popover').css('height',$('.popover').width()/3.15);
+        if($(window).width() > 768) {
+            $('.popover').css('height',$('.popover').width()/3.15);
+        }
     });
-    $('.popover').css('height',$('.popover').width()/3.15);
+    if($(window).width() > 768) {
+        $('.popover').css('height',$('.popover').width()/3.15);
+    }
 });
