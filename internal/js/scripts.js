fdafa291bd53ac323f32c8035331535b8d5e3a14
Nelson Pecora <nelson@yoshokatana.com>
2013-04-05 18:02:22 -0400

    fixed video size, added links

--- internal/js/scripts.js
+++ internal/js/scripts.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
         if($(window).width() > 768) {
             $('.popover').css('height',$('.popover').width()/3.15);
         }
+        $('#VideoPopup').css('height',$('#VideoPopup').width()/1.78);
     });
     if($(window).width() > 768) {
         $('.popover').css('height',$('.popover').width()/3.15);
     }
+    $('#VideoPopup').css('height',$('#VideoPopup').width()/1.78);
 });
